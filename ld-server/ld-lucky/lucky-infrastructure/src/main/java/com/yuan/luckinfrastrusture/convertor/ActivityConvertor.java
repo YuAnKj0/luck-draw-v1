a12f75e38958a6c8640caa2a9165bbdc17968e4b
YuAnKj0 <1614350118@qq.com>
2023-04-23 10:32:12 +0800

    奖项+活动信息CRUD

--- ld-server/ld-lucky/lucky-infrastructure/src/main/java/com/yuan/luckinfrastrusture/convertor/ActivityConvertor.java
+++ ld-server/ld-lucky/lucky-infrastructure/src/main/java/com/yuan/luckinfrastrusture/convertor/ActivityConvertor.java
@@ -1,6 +1,7 @@
 package com.yuan.luckinfrastrusture.convertor;
 
 import com.yuan.luckdomain.activity.ActivityEntity;
+import com.yuan.luckdomain.activity.ActivityTime;
 import com.yuan.luckinfrastrusture.gateway.impl.dataobject.ActivityDB;
 
 /**
@@ -12,8 +13,29 @@ import com.yuan.luckinfrastrusture.gateway.impl.dataobject.ActivityDB;
 
 public class ActivityConvertor {
    public static ActivityDB toActivityDB(ActivityEntity entity) {
+       ActivityDB activityDB = new ActivityDB();
+       activityDB.setId(entity.getId());
+       activityDB.setActivityName(entity.getActivityName());
+       activityDB.setStartTime(entity.getActivityTime().getStartTime());
+       activityDB.setEndTime(entity.getActivityTime().getEndTime());
+       activityDB.setDescribe(entity.getDescribe());
+       activityDB.setCreateTime(entity.getCreateTime());
+       activityDB.setCreator(entity.getCreator());
+       activityDB.setUpdateTime(entity.getUpdateTime());
+       activityDB.setUpdater(entity.getUpdater());
+       return activityDB;
    }
    
    public static ActivityEntity toEntity(ActivityDB activityDB) {
+       ActivityEntity activityEntity = new ActivityEntity();
+       activityEntity.setId(activityDB.getId());
+       activityEntity.setActivityName(activityDB.getActivityName());
+       activityEntity.setActivityTime(new ActivityTime(activityDB.getStartTime(),activityDB.getEndTime()));
+       activityEntity.setDescribe(activityDB.getDescribe());
+       activityEntity.setCreateTime(activityDB.getCreateTime());
+       activityEntity.setCreator(activityDB.getCreator());
+       activityEntity.setUpdateTime(activityDB.getUpdateTime());
+       activityEntity.setUpdater(activityDB.getUpdater());
+       return activityEntity;
    }
 }
